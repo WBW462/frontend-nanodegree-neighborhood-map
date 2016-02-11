6c8c7bcdb265ffd7d08c3025c7b2d0d61f0b9a71
Bill Whitlow <wbw462@bellsouth.net>
2016-02-10 20:32:09 -0500

    Added markers
    Added markers and reduced #map from 100 to 70%, in CSS.

--- js/app.js
+++ js/app.js
@@ -5,45 +5,192 @@ var marker;
 
 // initializes map
 function initMap() {
-//  var myLatLng = {lat: 61.197798, lng: -149.785479};
-  var myLatLng = {lat: 33.7490631, lng: -84.3881399};
+    //  var myLatLng = {lat: 61.197798, lng: -149.785479};
+    var myLatLng = {
+        lat: 33.7490631,
+        lng: -84.3881399
+    };
+    var abgLatLng = {
+        lat: 33.789203,
+        lng: -84.372756
+    };
+    var darbLatLng = {
+        lat: 33.919190,
+        lng: -84.504063
+    };
+    var euLatLng = {
+        lat: 33.789441,
+        lng: -84.326844
+    };
+    var fmonhLatLng = {
+        lat: 33.774000,
+        lng: -84.327745
+    };
+    var tftLatLng = {
+        lat: 33.772620,
+        lng: -84.385561
+    };
+    var giotLatLng = {
+        lat: 33.775339,
+        lng: -84.396227
+    };
+    var mcLatLng = {
+        lat: 33.745941,
+        lng: -84.413834
+    };
+    var hjaiaLatLng = {
+        lat: 33.640863,
+        lng: -84.444379
+    };
+    var sfogLatLng = {
+        lat: 33.769882,
+        lng: -84.547636
+    };
+    var smtpLatLng = {
+        lat: 33.802728,
+        lng: -84.155124
+    };
+    var tvLatLng = {
+        lat: 33.771510,
+        lng: -84.389311
+    };
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        scrollwheel: true,
+        zoom: 11
+    });
 
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    scrollwheel: true,
-    zoom: 11
-  });
-
-  // Create a marker and set its position.
+    // Create a marker and set its position.
     marker = new google.maps.Marker({
-    map: map,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    position: myLatLng,
-    title: 'Where I lived, while in the Air Force!'
-  });
-  marker.addListener('click', toggleBounce);
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: myLatLng,
+        title: 'Georgia Capital'
+    });
+
+    // Create a marker and set its position.
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: abgLatLng,
+        title: 'Atlanta Botanical Garden'
+    });
+
+    // Create a marker and set its position.
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: darbLatLng,
+        title: 'Dobbins Air Reserve Base'
+    });
+
+    // Create a marker and set its position.
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: euLatLng,
+        title: 'Emory University'
+    });
+
+    // Create a marker and set its position.
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: fmonhLatLng,
+        title: 'Fernbank Museum Of Natural History'
+    });
+
+    // Create a marker and set its position.
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: tftLatLng,
+        title: 'The Fox Theatre'
+    });
+
+    // Create a marker and set its position.
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: giotLatLng,
+        title: 'Georgia Institute of Technology'
+    });
+
+    // Create a marker and set its position.
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: mcLatLng,
+        title: 'Morehouse College'
+    });
+
+    // Create a marker and set its position.
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: hjaiaLatLng,
+        title: 'Hartsfield-Jackson Atlanta International Airport'
+    });
+
+    // Create a marker and set its position.
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: sfogLatLng,
+        title: 'Six Flags Over Georgia'
+    });
+
+    // Create a marker and set its position.
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: smtpLatLng,
+        title: 'Stone Mountain Theme Park'
+    });
+
+    // Create a marker and set its position.
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: tvLatLng,
+        title: 'The Varsity'
+    });
+
+    marker.addListener('click', toggleBounce);
 }
 
 function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
-  for (var x = 0; x < 5; x++) {
-  //  marker.setAnimation(google.maps.Animation.BOUNCE);  NOT NEEDED???
-    stopAnimation(marker);
-  }
-  function stopAnimation(marker) {
-    setTimeout(function () {
-      marker.setAnimation(null);
-    }, 2250);
-  }
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+    for (var x = 0; x < 5; x++) {
+        //  marker.setAnimation(google.maps.Animation.BOUNCE);  NOT NEEDED???
+        stopAnimation(marker);
+    }
+
+    function stopAnimation(marker) {
+        setTimeout(function() {
+            marker.setAnimation(null);
+        }, 2250);
+    }
 }
 // returns array of locations for neighborhood maps
 //function locationFinder() {
-  // initializes an empty array
-  //var locations = [];
+// initializes an empty array
+//var locations = [];
 //}
