fb4438645c2550387a913eac3607cb96548a7e8b
Bill Whitlow <wbw462@bellsouth.net>
2016-02-29 13:43:32 -0500

    Broke out venues into their own file for easier swapping of venues
    Created venues.js, starting moving information from app.js.  Discovered
    two js libraries, one as js.lib and the other as js.libs.  Not sure how
    or what happen but I'm removing the js.libs and going with js.lib
    instead.

---
+++ js/venues.js
@@ -0,0 +1,65 @@
+// Begin Model...............
+
+//  Lat/Lng starting point and locatons of interest
+var venue = [{
+    name: "Georgia Capital",
+    lat: 33.7490631,
+    lng: -84.3881399,
+    url:
+}, {
+    name: "Atlanta Botanical Garden",
+    lat: 33.789203,
+    lng: -84.372756,
+    url: "http://atlantabg.org/"
+}, {
+    name: "Dobbins Air Reserve Base",
+    lat: 33.919190,
+    lng: -84.504063,
+    url: "http://www.dobbins.afrc.af.mil/"
+}, {
+    name: "Emory University",
+    lat: 33.789441,
+    lng: -84.326844,
+    url: "http://www.emory.edu/home/index.html"
+}, {
+    name: "Fernbank Museum Of Natural History",
+    lat: 33.774000,
+    lng: -84.327745,
+    url: "http://www.fernbankmuseum.org/"
+}, {
+    name: "The Fox Theatre",
+    lat: 33.772620,
+    lng: -84.385561,
+    url: "http://foxtheatre.org/"
+}, {
+    name: "Georgia Institute of Technology",
+    lat: 33.775339,
+    lng: -84.396227,
+    url: "http://www.gatech.edu/"
+}, {
+    name: "Morehouse College",
+    lat: 33.745941,
+    lng: -84.413834,
+    url: "http://www.morehouse.edu/"
+}, {
+    name: "Hartsfield-Jackson Atlanta International Airport",
+    lat: 33.640863,
+    lng: -84.444379,
+    url: "http://www.atlanta-airport.com/"
+}, {
+    name: "Six Flags Over Georgia",
+    lat: 33.769882,
+    lng: -84.547636,
+    url: "https://www.sixflags.com/overgeorgia"
+}, {
+    name: "Stone Mountain Theme Park",
+    lat: 33.802728,
+    lng: -84.155124,
+    url: "http://www.stonemountainpark.com/"
+}, {
+    name: "The Varsity",
+    lat: 33.771510,
+    lng: -84.389311,
+    url: "http://www.thevarsity.com/"
+}, ];
+// End Model.......
